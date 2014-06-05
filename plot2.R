ed5d1c5a86322607a7576647f2cf955dc9859533
csippeter <csippeter@users.noreply.github.com>
2014-06-05 11:54:00 +0200

    plot2.R

---
+++ plot2.R
@@ -0,0 +1,15 @@
+data<-read.csv("household_power_consumption.txt", sep=";")
+data1<-data[, c(1,2,3)]
+data1$Date2<-as.Date(data1$Date, "%d/%m/%Y")
+data2<-subset(data1, data1$Date2 == "2007-02-01" | data1$Date2 == "2007-02-02")
+data2$DateTime <- paste(data2$Date, data2$Time)
+data2$DateTime <- strptime(data2$DateTime, format = "%d/%m/%Y %H:%M:%S")
+gap <- as.numeric(paste(data2$Global_active_power))
+#quartz() # in Mac
+#windows() #in Windows
+x11() #Linux
+plot(data2$DateTime, gap, type="l", xlab=" ", ylab="Global Active Power (kilowatts)")
+
+savePlot(filename = paste("plot2.png"),
+         type = "png",
+         device = dev.cur())
